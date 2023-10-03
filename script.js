f955c0655bfa6e9d14bdd635c1e969c523c9ca0d
pablinhoocl <xpabliito@gmail.com>
2023-10-02 21:55:21 -0700

    se agrego la mayoria de funciones restantes, solo falta el ai, terminar el html, con el selector de dificultad y boton restar, ademas de la funcion restart

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //      GAME BOARD
-gameBoard= [];
-playerId= 'x';
+let gameBoard= [],
+playerId= 'x',
+cpuId= 'o',
+gameResult='',
+whoWin= '',
+winner;
 const gameBoardAdd= (() =>{
 
     const arrayAdder= (x) => gameBoard.push(document.getElementById(x));
@@ -20,15 +24,78 @@ const displayController= (() =>{
 
     const playerSelect = (select) =>  {         //        PLAYER SELECT
         if( select== 'x'){
-            playerId= 'x';
+            playerId= 'x'; cpuId= 'o'
         }else if( select== 'o'){
-            playerId= 'o';
-        };}
+            playerId= 'o'; cpuId= 'x';
+        };};
+
+        const drawChecker= () =>{
+        let _drawCounter= 0;
+        for (let i= 0; i< gameBoard.length; i++){
+            let _checker= gameBoard[i].hasChildNodes();
+            if(_checker== true){
+                _drawCounter++
+            }else{console.log(_drawCounter)};
+            if(_drawCounter== 9){
+                gameResult= 'Draw'
+            }
+        }
+    }
     
+        const gameChecker = () => {
+            if ((gameBoard[0].innerText == 'x' && gameBoard[0].innerText === gameBoard[1].innerText && gameBoard[1].innerText === gameBoard[2].innerText)
+                || (gameBoard[0].innerText == 'o' && gameBoard[0].innerText === gameBoard[1].innerText && gameBoard[1].innerText === gameBoard[2].innerText)) { //ROW 1
+                winner = gameBoard[0].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[3].innerText == 'x' && gameBoard[3].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[5].innerText)
+                || (gameBoard[3].innerText == 'o' && gameBoard[3].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[5].innerText)) { //ROW 2
+                winner = gameBoard[3].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[6].innerText == 'x' && gameBoard[6].innerText === gameBoard[7].innerText && gameBoard[7].innerText === gameBoard[8].innerText)
+                || (gameBoard[6].innerText == 'x' && gameBoard[6].innerText === gameBoard[7].innerText && gameBoard[7].innerText === gameBoard[8].innerText)) { // ROW 3
+                winner = gameBoard[6].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[0].innerText == 'x' && gameBoard[0].innerText === gameBoard[3].innerText && gameBoard[3].innerText === gameBoard[6].innerText)
+                || (gameBoard[0].innerText == 'o' && gameBoard[0].innerText === gameBoard[3].innerText && gameBoard[3].innerText === gameBoard[6].innerText)) { //COLUMN 1
+                winner = gameBoard[0].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[1].innerText == 'x' && gameBoard[1].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[7].innerText)
+                || (gameBoard[1].innerText == 'o' && gameBoard[1].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[7].innerText)) { //COLUMN 2
+                winner = gameBoard[1].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[2].innerText == 'x' && gameBoard[2].innerText === gameBoard[5].innerText && gameBoard[5].innerText === gameBoard[8].innerText)
+                || (gameBoard[2].innerText == 'o' && gameBoard[2].innerText === gameBoard[5].innerText && gameBoard[5].innerText === gameBoard[8].innerText)) { //COLUMN 3
+                winner = gameBoard[2].innerText;
+                gameResult = 'Winner';
+
+            } else if ((gameBoard[0].innerText == 'x' && gameBoard[0].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[8].innerText)
+                || (gameBoard[0].innerText == 'o' && gameBoard[0].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[8].innerText)) { //DIAGONAL 1
+                winner = gameBoard[0].innerText;
+                gameResult = "Winner";
+
+            } else if ((gameBoard[2].innerText =='x' && gameBoard[2].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[6].innerText)
+                || (gameBoard[2].innerText =='o' && gameBoard[2].innerText === gameBoard[4].innerText && gameBoard[4].innerText === gameBoard[6].innerText)) { //DIAGONAL 2
+                winner = gameBoard[2].innerText;
+                gameResult = 'Winner';
+
+            }else{
+                drawChecker();
+                
+            };
+            console.log(gameResult)
+        };
+        
     
 
         return{
-            playerSelect
+            playerSelect,
+            gameChecker,
+            drawChecker
         }
     
 
@@ -40,14 +107,17 @@ const oSelector= document.getElementById('o');
 xSelector.onclick= function(){displayController.playerSelect('x')};
 oSelector.onclick= function(){displayController.playerSelect('o')};
 
-// se crea un elemnto p con el playerId ;   se actualiza el array solo?
+// se crea un elemnto p con el playerId ;   se actualiza el array solo?SIM
 
 const gameOn= (x) =>{
     const divSelector = () => document.getElementById(x);
     const pAdder= () => {
         newP= document.createElement('p');
         newP.innerHTML= playerId;
-        divSelector().append(newP);
+        if( divSelector().hasChildNodes()== true){
+          console.log(divSelector(), divSelector().hasChildNodes())
+        }
+        else{divSelector().append(newP);};
     };
     return {
         pAdder,
@@ -56,11 +126,46 @@ const gameOn= (x) =>{
 
 };
 
+//      AI PLAYER
+const aiPlayer = () => {
+    if (gameResult === '' && playerId !== cpuId) {
+        
+        const emptyCells = gameBoard.filter(cell => cell.innerText === '');
+
+        
+        if (emptyCells.length > 0) {
+            const randomIndex = Math.floor(Math.random() * emptyCells.length);
+            const selectedCell = emptyCells[randomIndex];
+
+            
+            selectedCell.innerText = cpuId;
+
+            
+            playerId = cpuId;
+
+            
+            displayController.gameChecker();
+            displayController.drawChecker();
+        }
+    }
+};
+
+
+
+
 function pOnClicker(x){
     let iDiv= gameOn(x);
     iDiv.pAdder();
-}
-for(let i= 1; i< 10; i++){
+};
+for(let i= 1; i< 10; i++){ //AGREGA LAS FUNCIONES NECESARIAS AL TABLERO
     let iSelector= document.getElementById(i);
-    iSelector.onclick= function (){pOnClicker(i)};
-    }
+    iSelector.onclick= function (){
+        pOnClicker(i),
+        displayController.gameChecker(),
+        displayController.drawChecker()
+    };
+    };
+function uno(){
+for (let i=0; i<gameBoard.length; i++){
+    console.log(gameBoard[i]);
+}}
